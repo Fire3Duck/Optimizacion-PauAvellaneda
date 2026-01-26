7b9291ca1a59c17fdb4f0617e5b7a32f70884bbc
Fire3Duck <pauavellanedalopez@gmail.com>
2026-01-26 09:29:28 +0100

    Bala

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    private Rigidbody _rigidbody;
+    [SerializeField] private float _bulletSpeed = 10f;
+
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _rigidbody.AddForce(transform.forward * _bulletSpeed, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        Destroy (gameObject);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Player : MonoBehaviour
+{
+    private InputAction _shootAction;
+
+    [SerializeField] private GameObject _bulletPrefab;
+    [SerializeField] private Transform _BulletSpawn;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _shootAction = InputSystem.actions["Attack"];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_shootAction.WasPerformedThisFrame())
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(_bulletPrefab, _BulletSpawn.position, _BulletSpawn.rotation);
+    }
+}
